7824d8c4cb48c691f0765ab2e63b2312270bc003
aertoria <castives@gmail.com>
2016-09-28 13:18:44 -0700

    2nd

--- ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/plus/HeimdallMetricReducerTestFULL.java
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/plus/HeimdallMetricReducerTestFULL.java
@@ -2679,6 +2679,185 @@ public class HeimdallMetricReducerTestFULL {
 	}
 	
 	@Test
+	public void HeimdallTotalAvaTransform_REPORT_RACHOUR(){
+		Transform transform=injector.getInstance(HeimdallMetricReducer.class);
+		int offset=1000*60;
+		long start=0L;
+		long hourstart=1000*3600L;
+        Metric metric_1 = new Metric(TEST_SCOPE, TEST_METRIC);
+        Map<Long, String> datapoints_1 = new HashMap<Long, String>();
+        datapoints_1.put(start+1L*offset, "00.0");
+        datapoints_1.put(start+2L*offset, "100.0");
+        datapoints_1.put(start+3L*offset, "100.0");
+        datapoints_1.put(start+4L*offset, "100.0");
+        datapoints_1.put(start+5L*offset, "40.0");
+        datapoints_1.put(start+6L*offset, "20.0");
+        datapoints_1.put(hourstart+1L*offset, "90.0");
+        datapoints_1.put(hourstart+2L*offset, "120.0");
+        datapoints_1.put(hourstart+3L*offset, "100.0");
+        datapoints_1.put(hourstart+4L*offset, "10.0");
+        datapoints_1.put(hourstart+5L*offset, "40.0");
+        datapoints_1.put(hourstart+6L*offset, "20.0");
+        metric_1.setDatapoints(datapoints_1);
+        metric_1.setTag("device", "na11-app1-1-chi.ops.sfdc.net");
+        metric_1.setMetric("SFDC_type-Stats-name1-System-name2-trustAptRequestTimeRACNode2.Last_1_Min_Avg");
+        
+        Metric metric_2 = new Metric(TEST_SCOPE, TEST_METRIC);
+        Map<Long, String> datapoints_2 = new HashMap<Long, String>();
+        datapoints_2.put(start+1L*offset, "100.0");
+        datapoints_2.put(start+2L*offset, "150.0");
+        datapoints_2.put(start+3L*offset, "130.0");
+        datapoints_2.put(start+4L*offset, "95.0");
+        datapoints_2.put(start+5L*offset, "90.0");
+        datapoints_2.put(start+6L*offset, "5.0");
+        datapoints_2.put(hourstart+1L*offset, "100.0");
+        datapoints_2.put(hourstart+2L*offset, "10.0");
+        datapoints_2.put(hourstart+3L*offset, "50.0");
+        datapoints_2.put(hourstart+4L*offset, "90.0");
+        datapoints_2.put(hourstart+5L*offset, "90.0");
+        datapoints_2.put(hourstart+6L*offset, "50.0");
+        metric_2.setDatapoints(datapoints_2);
+        metric_2.setTag("device", "na11-app1-2-chi.ops.sfdc.net");
+        metric_2.setMetric("SFDC_type-Stats-name1-System-name2-trustAptRequestTimeRACNode2.Last_1_Min_Avg");
+        
+        Metric metric_3 = new Metric(TEST_SCOPE, TEST_METRIC);
+        Map<Long, String> datapoints_3 = new HashMap<Long, String>();
+        datapoints_3.put(start+1L*offset, "2.0");
+        datapoints_3.put(start+2L*offset, "3.0");
+        datapoints_3.put(start+3L*offset, "3.0");
+        datapoints_3.put(start+4L*offset, "2.0");
+        datapoints_3.put(start+5L*offset, "4.0");
+        datapoints_3.put(start+6L*offset, "2.0");
+        datapoints_3.put(hourstart+1L*offset, "2.0");
+        datapoints_3.put(hourstart+2L*offset, "3.0");
+        datapoints_3.put(hourstart+3L*offset, "3.0");
+        datapoints_3.put(hourstart+4L*offset, "2.0");
+        datapoints_3.put(hourstart+5L*offset, "4.0");
+        datapoints_3.put(hourstart+6L*offset, "2.0");
+        metric_3.setDatapoints(datapoints_3);
+        metric_3.setTag("device", "na11-app1-1-chi.ops.sfdc.net");
+        metric_3.setMetric("SFDC_type-Stats-name1-System-name2-trustAptRequestCountRACNode2.Last_1_Min_Avg");
+        
+        Metric metric_4 = new Metric(TEST_SCOPE, TEST_METRIC);
+        Map<Long, String> datapoints_4 = new HashMap<Long, String>();
+        datapoints_4.put(start+1L*offset, "1.0");
+        datapoints_4.put(start+2L*offset, "9.0");
+        datapoints_4.put(start+3L*offset, "2.0");
+        datapoints_4.put(start+4L*offset, "2.0");
+        datapoints_4.put(start+5L*offset, "9.0");
+        datapoints_4.put(start+6L*offset, "1.0");
+        datapoints_4.put(hourstart+1L*offset, "1.0");
+        datapoints_4.put(hourstart+2L*offset, "9.0");
+        datapoints_4.put(hourstart+3L*offset, "2.0");
+        datapoints_4.put(hourstart+4L*offset, "2.0");
+        datapoints_4.put(hourstart+5L*offset, "9.0");
+        datapoints_4.put(hourstart+6L*offset, "1.0");
+        metric_4.setDatapoints(datapoints_4);
+        metric_4.setTag("device", "na11-app1-2-chi.ops.sfdc.net");
+        metric_4.setMetric("SFDC_type-Stats-name1-System-name2-trustAptRequestCountRACNode2.Last_1_Min_Avg");
+        
+        
+        Metric metric_5 = new Metric(TEST_SCOPE, TEST_METRIC);
+        Map<Long, String> datapoints_5 = new HashMap<Long, String>();
+        datapoints_5.put(start+1L*offset, "22.0");
+        datapoints_5.put(start+2L*offset, "95.0");
+        datapoints_5.put(start+3L*offset, "67.0");
+        datapoints_5.put(start+4L*offset, "57.0");
+        datapoints_5.put(start+5L*offset, "75.0");
+        datapoints_5.put(start+6L*offset, "55.0");
+        metric_5.setDatapoints(datapoints_5);
+        metric_5.setTag("device", "na11-app1-1-chi.ops.sfdc.net");
+        metric_5.setMetric("SFDC_type-Stats-name1-System-name2-trustAptRequestTimeRACNode1.Last_1_Min_Avg");
+     
+        
+        Metric metric_6 = new Metric(TEST_SCOPE, TEST_METRIC);
+        Map<Long, String> datapoints_6 = new HashMap<Long, String>();
+        datapoints_6.put(start+1L*offset, "4.0");
+        datapoints_6.put(start+2L*offset, "5.0");
+        datapoints_6.put(start+3L*offset, "6.0");
+        datapoints_6.put(start+4L*offset, "7.0");
+        datapoints_6.put(start+5L*offset, "8.0");
+        datapoints_6.put(start+6L*offset, "4.0");
+        metric_6.setDatapoints(datapoints_6);
+        metric_6.setTag("device", "na11-app1-1-chi.ops.sfdc.net");
+        metric_6.setMetric("SFDC_type-Stats-name1-System-name2-trustAptRequestCountRACNode1.Last_1_Min_Avg");
+        
+        List<Metric> metrics = new ArrayList<Metric>();
+        metrics.add(metric_1);
+        metrics.add(metric_2);
+        metrics.add(metric_3);
+        metrics.add(metric_4);
+        metrics.add(metric_5);
+        metrics.add(metric_6);    
+        
+        //ACT
+        Metric metric_act1 = new Metric(TEST_SCOPE_ACT, TEST_METRIC);
+        Map<Long, String> datapoints_act1 = new HashMap<Long, String>();
+        datapoints_act1.put(start+1L*offset, "42.0");
+        datapoints_act1.put(start+2L*offset, "1.0");
+        datapoints_act1.put(start+3L*offset, "19.0");
+        datapoints_act1.put(start+4L*offset, "2.0");
+        datapoints_act1.put(start+5L*offset, "12.0");
+        datapoints_act1.put(start+6L*offset, "23.0");
+        metric_act1.setDatapoints(datapoints_act1);
+        metric_act1.setTag("device", "na11-db1-1-chi.ops.sfdc.net");
+        metric_act1.setMetric("CNADB11.NADB11-1.active__sessions");
+     
+        metrics.add(metric_act1);    
+        
+        //CPUSYS
+        Metric metric_cpuSys = new Metric(TEST_SCOPE_CPU, TEST_METRIC);
+        Map<Long, String> datapoints_cpuSys = new HashMap<Long, String>();
+        datapoints_cpuSys.put(start+1L*offset, "1.05");
+        datapoints_cpuSys.put(start+2L*offset, "10.3");
+        datapoints_cpuSys.put(start+3L*offset, "40.9");
+        datapoints_cpuSys.put(start+4L*offset, "66.0");
+        datapoints_cpuSys.put(start+5L*offset, "1.0");
+        datapoints_cpuSys.put(start+6L*offset, "2.0");
+        metric_cpuSys.setDatapoints(datapoints_cpuSys);
+        metric_cpuSys.setTag("device", "na11-db1-1-chi.ops.sfdc.net");
+        metric_cpuSys.setMetric("CpuPerc.cpu.system");
+        metrics.add(metric_cpuSys);    
+        
+        //CPU-empty-doubledigit-no par APT
+        Metric metric_cpuSys2 = new Metric(TEST_SCOPE_CPU, TEST_METRIC);
+        metric_cpuSys2.setTag("device", "na11-db1-12-chi.ops.sfdc.net");
+        metric_cpuSys2.setMetric("CpuPerc.cpu.system");
+        metrics.add(metric_cpuSys2);    
+        
+        //CPUUSER
+        Metric metric_cpuUser = new Metric(TEST_SCOPE_CPU, TEST_METRIC);
+        Map<Long, String> datapoints_cpuUser = new HashMap<Long, String>();
+        datapoints_cpuUser.put(start+1L*offset, "1.05");
+        datapoints_cpuUser.put(start+2L*offset, "33.3");
+        datapoints_cpuUser.put(start+3L*offset, "10");
+        datapoints_cpuUser.put(start+4L*offset, "4.0");
+        datapoints_cpuUser.put(start+5L*offset, "1.0");
+        datapoints_cpuUser.put(start+6L*offset, "2.0");
+        metric_cpuUser.setDatapoints(datapoints_cpuUser);
+        metric_cpuUser.setTag("device", "na11-db1-1-chi.ops.sfdc.net");
+        metric_cpuUser.setMetric("CpuPerc.cpu.user");
+        metrics.add(metric_cpuUser);    
+        
+        
+        
+        List<Metric> result = transform.transform(metrics,Arrays.asList("RACHOUR"));
+
+        
+        List<Metric> expected = new ArrayList<Metric>();
+        Metric expected_metric1 = new Metric("AVA", "CHI.SP2.cs15.Rac1");
+        Map<Long, String> expected_Datapoints1=new HashMap<Long, String>();
+        expected_Datapoints1.put(0L, "100.0");
+        expected_Datapoints1.put(3600000L, "100.0");
+        expected_metric1.setDatapoints(expected_Datapoints1);
+        expected.add(expected_metric1);
+
+        System.out.println(result);
+        assertEquals(expected.get(0),result.get(0)); 
+        assertEquals(result.get(0).getDatapoints(),expected.get(0).getDatapoints()); 
+	}
+	
+	@Test
 	public void HeimdallTotalAvaTransform_APT_WITH_EMPTY_ACT(){
 		Transform transform=injector.getInstance(HeimdallMetricReducer.class);
 		int offset=1000*60;
@@ -2879,5 +3058,4 @@ public class HeimdallMetricReducerTestFULL {
         assertEquals(expected,result); 
         assertEquals(result.get(0).getDatapoints(),expected.get(0).getDatapoints()); 
 	}
-
 }
